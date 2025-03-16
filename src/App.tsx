9b9cae45c41e4895ae096e9fca1d92a434354818
bagasafrz7 <bagasafrz16@gmail.com>
2025-03-16 15:53:58 +0700

    feat: add base name

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
   <TooltipProvider>
    <Toaster />
    <Sonner />
-   <BrowserRouter>
+   <BrowserRouter basename="/github-explorer-repositories">
     <Routes>
      <Route path="/" element={<Index />} />
      <Route path="*" element={<NotFound />} />
